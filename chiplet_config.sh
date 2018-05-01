d136efa173e949382da679d0a8847f4e0aad226d
David kang <dkang@isi.edu>
2018-05-01 07:58:54 -0700

    Now rootfile system is configured to be detached from the kernel.

--- chiplet_config.sh
+++ chiplet_config.sh
@@ -1,2 +1,3 @@
-make ARCH=arm64 xilinx_zynqmp_defconfig CONFIG_INITRAMFS_SOURCE=./rootfs.cpio
+make ARCH=arm64 xilinx_zynqmp_defconfig 
+#make ARCH=arm64 xilinx_zynqmp_defconfig CONFIG_INITRAMFS_SOURCE=./rootfs.cpio
 #make ARCH=arm64 menuconfig
